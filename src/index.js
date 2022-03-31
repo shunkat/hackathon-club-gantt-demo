a95b8d50dbfcc99cac5b80ba6c522dfe6fdf36a0
shunkat <shunkat@icloud.com>
2022-04-01 00:36:29 +0900

    もうだめ

--- src/index.js
+++ src/index.js
@@ -1,17 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import FrappeGantt from "./components/FrappeGantt.jsx";
+import Task from "./lib/Task.js";
+import ViewMode from "./lib/ViewMode.js";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+export { FrappeGantt, Task, ViewMode };
